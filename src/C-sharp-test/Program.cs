3c81133972a3d9cf5af6654c09702c065cc66853
Dragan Jovanovic <draganjovanovic1@gmail.com>
2017-01-06 10:39:16 +0100

    C# and F# console apps with build and debug tasks

---
+++ src/C-sharp-test/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace C_sharp_test
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+        }
+    }
+}
---
+++ src/F-sharp-test/Program.fs
@@ -0,0 +1,6 @@
+open System
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%s" <| DateTime.Now.ToString ()
+    0
